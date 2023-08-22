52f651890bd0dfb2011368051f178307ea15634b
GianbaBotassi <gianba89@gmail.com>
2023-08-22 13:40:31 +0200

    Fix class pages, axios call only names

---
+++ src/components/Pages.vue
@@ -0,0 +1,21 @@
+<template>
+  <a v-html="link.label" @click="$emit('load')" :class="{
+    'bg-secondary': link.active,
+    'disabled': link.url == null
+  }" class="text-decoration-none text-dark px-3 py-1 rounded">
+  </a>
+</template>
+<script>
+export default {
+  name: 'Page',
+  props: {
+    link: Object
+  }
+}
+</script>
+<style>
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,54 +1,64 @@
 <script>
 import axios from 'axios';
 
+import ProjectCard from './ProjectCard.vue'
+import Pages from './Pages.vue'
+
 export default {
   name: 'Project',
+  components: {
+    ProjectCard,
+    Pages
+  },
   data() {
     return {
       projectUrl: 'http://localhost:8000/api/projects',
-      projects: [],
+      projectsName: [],
       links: ''
     }
   },
   methods: {
-    loadProject(index) {
-      axios.get(index)
+    loadPage(index) {
+      if (index == null) return;
+      this.loadProjects(index)
+    },
+    loadProjects(target) {
+      axios.get(target)
         .then(response => {
-          this.projects = response.data.projects.data;
-          console.log(response.data.projects);
+          // Get links della paginazione
+          this.links = response.data.projects.links;
+          this.projectsName = [];
+          // Ciclo for per nomi progetti
+          const data = response.data.projects.data;
+          data.forEach(project => {
+            this.projectsName.push(project.name)
+          });
+          console.log(JSON.stringify(this.projectsName));
         })
         .catch(error => {
           console.log('error: ', error)
         })
-      console.log(index);
     }
   },
   mounted() {
-    axios.get(this.projectUrl)
-      .then(response => {
-        this.projects = response.data.projects.data;
-        this.links = response.data.projects.links
-        console.log(this.links);
-      })
-      .catch(error => {
-        console.log('error: ', error)
-      })
+    this.loadProjects(this.projectUrl)
   }
 }
 </script>
 
 <template>
   <h1>Progetti</h1>
-  <div v-for="project in projects" :key="project.id">
-    <div>
-      <a href="#" class="text-dark text-decoration-none">
-        [{{ project.id }}] {{ project.name }}
-      </a>
-    </div>
+  <div v-for="(project, index) in projectsName" :key="index">
+    {{ project.charAt(0).toUpperCase() + project.slice(1) }}
   </div>
-  <div class="d-flex justify-content-center gap-3 mt-3">
-    <a class="text-dark text-decoration-none" v-for="(link, index) in links" :key="index" v-html="link.label"
-      @click="loadProject(link.url)">
-    </a>
+  <div class="d-flex gap-3 mt-3">
+    <Pages v-for="(link, index) in links" :key="index" @load="loadPage(link.url)" :link="link"
+      :class="link.active ? 'bg-secondary text-white' : ''" />
   </div>
 </template>
+
+<style>
+a {
+  cursor: pointer;
+}
+</style>
