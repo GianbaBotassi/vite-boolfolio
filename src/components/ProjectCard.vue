b2fbc8182ebbc408dddd0a80b4a7e9e5b5ffbd10
GianbaBotassi <gianba89@gmail.com>
2023-08-22 15:14:01 +0200

    Set axios call for single proj and basic style

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,11 +1,36 @@
 <template>
-  <h1>sono project card</h1>
-  <router-link :to="{ name: 'home' }" class="btn btn-primary">Back to home</router-link>
+  <div class="container text-center">
+    <h1>{{ project.name }}</h1>
+    <p>{{ project.description }}</p>
+
+    <router-link :to="{ name: 'home' }" class="btn btn-primary mt-4">Back to home</router-link>
+  </div>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
   name: "ProjectCard",
-
+  data() {
+    return {
+      singleProjectUrl: 'http://localhost:8000/api/project-show/',
+      project: {}
+    }
+  },
+  methods: {
+    loadProjects(target) {
+      axios.get(target)
+        .then(response => {
+          this.project = response.data.project;
+        })
+        .catch(error => {
+          console.log('error: ', error)
+        })
+    }
+  },
+  mounted() {
+    this.loadProjects(this.singleProjectUrl + this.$route.params.id)
+  }
 }
 </script>
 <style></style>
